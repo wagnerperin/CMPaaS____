4d6d0a72e8cd4f1bf78da9cebf66ffb9a3c56c4e
wagnerperin <wagnerperin@gmail.com>
2022-07-05 15:36:57 -0300

    Renaming database to mongo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./conf/app');
 const env = require('./conf/env');
-const database = require('./conf/mongo');
+const mongo = require('./conf/mongo');
 
-database.connect(env.DBURI);
+mongo.connect(env.DBURI);
 
 app.listen(env.PORT, env.HOST, () => {
   console.log(`Server running at http://${env.HOST}:${env.PORT}/`);
