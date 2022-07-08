1fdc08bdcb9a4aa77a32b1ad754488642feba110
wagnerperin <wagnerperin@gmail.com>
2022-07-08 16:24:59 -0300

    Implementing authenticationRequired

--- app/apis/auth.js
+++ app/apis/auth.js
@@ -4,9 +4,32 @@ module.exports = app => {
   const userModel = require('mongoose').model('User');
   const api = {};
 
+  const loadToken = (req) => {
+    req.isAuthenticated = () => req.user ? true : false;
+
+    const token = req.headers['x-access-token'];
+
+    return new Promise((resolve, reject) => {
+      if(token) {
+        jwt.verify(token, env.JWT_AUTH_SECRET, (err, decoded) => {
+          if(err) {
+            reject(err);
+          }else{
+            req.user = decoded;
+            resolve(decoded);
+          }
+        });
+      } else {
+        reject({err: 'No token provided'});
+      }
+    });
+  
+  };
+
   api.authenticate = async (req, res) => {
     const {email, password} = req.body;
     const user = await userModel.findOne({email}).select('+password +salt');
+
     if(!user) {
       res.status(401).json({error: 'User not found'});
     }else{
@@ -14,9 +37,21 @@ module.exports = app => {
       if(!isValid){
         res.status(401).json({error: 'Invalid password'});
       }else{
-        const token = jwt.sign({id: user._id}, env.JWT_AUTH_SECRET);
+        const token = jwt.sign({id: user._id}, env.JWT_AUTH_SECRET, {expiresIn: '1d'});
         res.json({'x-access-token': token});
       }
+    }
+  };
+
+  api.authenticationRequired = async (req, res, next) => {
+    try{
+      await loadToken(req);
+
+      if(!req.isAuthenticated()) res.status(401).json({error: 'Authentication required'});
+
+      next();
+    }catch{
+      res.status(401).json({error: 'Authentication token required'});
     }
   };
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,8 +1,9 @@
 module.exports = app => {
   const api = app.apis.user;
+  const auth = app.apis.auth;
   const validateRequest = app.middlewares.schemaValidator(process.env.NODE_ENV == 'development');
   app
       .route('/user')
         .post(validateRequest, api.create)
-        .get(api.list);
+        .get(auth.authenticationRequired, api.list);
 }
